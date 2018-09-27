134d943d3d6a1c7173a36c86b8331eec2b8d01fb
Juan David Torres Vasquez <jdtorres31@gmail.com>
2018-09-27 17:56:34 -0500

    Se crea clase de productos

---
+++ SolutionXamarin/Sales.Common/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales.Common.Models
+{
+    class Product
+    {
+        public int ProductId { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsAvailable { get; set; }
+        public DateTime MyProperty { get; set; }
+
+    }
+}
